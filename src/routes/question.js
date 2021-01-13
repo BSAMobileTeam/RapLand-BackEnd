9065cf23e9bef63ba6d3f1e7e1da1875cb034c99
Froissart Kévin <kevin.froissart69@gmail.com>
2021-01-13 20:39:36 +0100

    middleware apiCheck with dotenv

--- src/routes/question.js
+++ src/routes/question.js
@@ -3,18 +3,18 @@ const sequelize = require('../sequelize')
 module.exports = app => {
     const controller = require('../controllers/question')
     const router = require('express').Router()
- 
-    router.post('/create', controller.create)
+
+    router.post('/create', [controller.apiKeyCheck, controller.create])
 
     router.get('/getById', controller.getById)
 
-    router.get('/getAll', controller.getAll)
+    router.get('/getAll', [controller.apiKeyCheck, controller.getAll])
 
     router.get('/getMixedArray', controller.getMixedArray)
 
-    router.post('/createWithArray', controller.createWithArray)
+    router.post('/createWithArray', [controller.apiKeyCheck, controller.createWithArray])
 
-    router.delete('/deleteById', controller.deleteById)
+    router.delete('/deleteById', [controller.apiKeyCheck, controller.deleteById])
 
     app.use('/question', router)
 }
